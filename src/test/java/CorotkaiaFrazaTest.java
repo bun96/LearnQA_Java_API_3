fa173d64e169203e87fcd41a4732147a9faba43f
Mikhail Lyubeznov <trudqwert123@gmail.com>
2024-02-11 10:04:49 +0300

    fraza_test

---
+++ src/test/java/CorotkaiaFrazaTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CheckLengthOfPhrase {
+
+    @ParameterizedTest
+    @ValueSource (strings = {"HelloSomeBody", "HelloSomeBodyToday"}) // HelloSomeBody are 13 symbols and HelloSomeBodyToday are 17 symbols
+
+    public void checkLengthOfPhrase(String phrase) {
+        String expectedName = (phrase.length() > 15) ? phrase : "";
+        assertEquals("HelloSomeBodyToday",expectedName, "Phrase more shortly when 15 characters");
+    }
+}
